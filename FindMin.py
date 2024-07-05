285c7024f676a9b386e1eaa8fad2c3faa5200ebc
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-07-04 20:03:54 -0600

    Create FindMin.py

---
+++ FindMin.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        sortednums = sorted(nums)
+        start, end = 0, len(nums) - 1
+        ans = nums[0]
+
+        while start <= end:
+            if nums[start] < nums[end]:
+                ans = min(ans, nums[start])
+                break
+
+            mid = (start + end) // 2
+            ans = min(ans, nums[mid])
+            if nums[mid] >= nums[start]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        return ans
