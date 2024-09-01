5b5790f0566f46c1a0a004bbcf7df5c730ff4422
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-08-31 19:29:53 -0600

    Create Permutations2.py

---
+++ Permutations2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(nums, path):
+            if not nums:
+                res.append(path.copy())
+
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+                dfs(nums[:i] + nums[i + 1:], path + [nums[i]])
+        
+        dfs(nums, [])
+        return res
